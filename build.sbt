4de3238467af3a437451f2866c91a11a182e095a
Ranadip Chatterjee <ranadip.c@gmail.com>
2023-09-08 23:33:51 +0100

    Minimised to smallest set of changes

--- build.sbt
+++ build.sbt
@@ -2,5 +2,3 @@ name := "HelloTestSbt"
 version := "0.1.0"
 scalaVersion := "2.12.18"
 
-// libraryDependencies += "org.apache.spark" %% "spark-core" % "3.3.0" % "provided"
-// libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.3.0" % "provided"
---
+++ src/main/scala/com/google/ranadip/HelloWorld.scala
@@ -0,0 +1,21 @@
+// Commenting this line replicates the problem. Uncommenting creates a working jar
+// package com.google.ranadip
+import com.google.ranadip.PrintMyName.printName
+
+
+// define main method (Spark entry point)
+object HelloWorld {
+  def main(args: Array[String]) {
+ 
+  
+    // do stuff
+    println("************")
+    println("************")
+    println("Hello, world!")
+    printName
+    println("************")
+    println("************")
+    
+  }
+}
+
